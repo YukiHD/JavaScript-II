c24449f4beca9ff1d8a7a0adcc1f6402e75bb7bf
Agata B <agatabubela@gmail.com>
2019-10-28 20:09:12 +0100

    done callbacks without stretch

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,6 +1,7 @@
-// Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
+// Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.
+// Study both the problem and the solution to figure out the rest of the problems.
 
-const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
+const items = ["Pencil", "Notebook", "yo-yo", "Gum"];
 
 /* 
 
@@ -25,8 +26,8 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
   // TEST 1 (inlined callback):
 
-  const test1 = firstItem(items, item => `I love my ${item}!`);
-  console.log(test1); // "I love my Pencil!"
+  // const test1 = firstItem(items, item => `I love my ${item}!`);
+  // console.log(test1); // "I love my Pencil!"
 
   // TEST 2 (declaring callback before hand):
 
@@ -38,27 +39,75 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
   console.log(test2); // "this Pencil is worth a million dollars!"
 */
 
+// ___________________________________________________________________________________
 
-function getLength(arr, cb) {
-  // getLength passes the length of the array into the callback.
-}
+// function getLength(arr, cb) {
+//   return cb(arr);
+//   // getLength passes the length of the array into the callback.
+// }
+// function arrLength(arr) {
+//   return arr.length;
+// }
 
-function last(arr, cb) {
-  // last passes the last item of the array into the callback.
-}
+// console.log(getLength(items, arrLength));
 
-function sumNums(x, y, cb) {
-  // sumNums adds two numbers (x, y) and passes the result to the callback.
-}
+// __________________________________________________________________________________
 
-function multiplyNums(x, y, cb) {
-  // multiplyNums multiplies two numbers and passes the result to the callback.
-}
+// function last(arr, cb) {
+//   // last passes the last item of the array into the callback.
+//   return cb(arr);
+// }
 
-function contains(item, list, cb) {
-  // contains checks if an item is present inside of the given array/list.
-  // Pass true to the callback if it is, otherwise pass false.
-}
+// function lastItem(arr) {
+//   return arr[arr.length - 1];
+// }
+
+// console.log(last(items, lastItem));
+
+// _________________________________________________________________________________
+
+// function sumNums(x, y, cb) {
+//   return cb(x, y);
+//   // sumNums adds two numbers (x, y) and passes the result to the callback.
+// }
+
+// function addNums(x, y) {
+//   return x + y;
+// }
+
+// console.log(sumNums(2, 3, addNums));
+
+// _____________________________________________________________________________________
+
+// function multiplyNums(x, y, cb) {
+//   return cb(x, y);
+//   // multiplyNums multiplies two numbers and passes the result to the callback.
+// }
+
+// function multiply(x, y) {
+//   return x * y;
+// }
+
+// console.log(multiplyNums(3, 4, multiply));
+
+// _____________________________________________________________________________________
+
+// function contains(item, list, cb) {
+//   return cb(item, list);
+// }
+
+// function insideList(item, list) {
+//   if (list.includes(item) === true) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+// console.log(contains("Gum", items, insideList));
+
+// _________________________________________________________________
+// contains checks if an item is present inside of the given array/list.
+// Pass true to the callback if it is, otherwise pass false.
 
 /* STRETCH PROBLEM */
 
